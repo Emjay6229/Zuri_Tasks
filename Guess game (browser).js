d25f762c9a5ed79bb508633718647b99040703a3
Emjay6299 <emjay6299@gmail.com>
2022-10-22 03:13:11 +0200

    This is my first Zuri code project

---
+++ Guess game (browser).js
@@ -0,0 +1,48 @@
+function guessGame(min, max) {
+
+    let about_game = `GuessIt is a Multi-level number guessing game. You are required to guess a randomly generated number. At a correct guess, you are awarded a point and moved to next level by increasing the maximum range value by 1.`
+    
+    let userName = (prompt(`Hi! Please enter your username. `));
+    
+    alert(`Hello ${userName}! Welcome to GuessIt Game!`);
+    alert(about_game);
+
+    let randomValue;
+    let userGuess;
+    let point = 0;
+    let level = 1;
+    let attempts = 5;
+
+     do {
+            randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
+            userGuess = parseInt(prompt(`Try to guess a number between ${min} and ${max}. You have ${attempts} attempts. `)); point++;
+
+            if (userGuess === randomValue && point > 1)
+                {
+                    level++
+                    alert(`Your guess is correct! You have ${point} points!`);
+                    alert(`Welcome to level ${level}.`)
+                    max++;
+                } 
+            else if (userGuess === randomValue && point === 1) 
+                { 
+                    level++
+                    alert(`Your guess is correct! You have ${point} point!`);
+                    alert(`Welcome to level ${level}.`)
+                    max++;
+                } 
+            else 
+                {
+                    for (attempts = 4; attempts > 0; attempts--)
+                        {
+                            alert(`Your guess was wrong! ${attempts} left.`);
+                            randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
+                            userGuess = parseInt(prompt(`Guess a number between ${min} and ${max}.`));
+                        }
+                    alert (`Game Over!!`);
+                    break;
+                }
+        } while (max < 11)
+    }
+
+guessGame(1, 2);
