a766c9199862bc489ea385d13d782ea7b3aba8d8
Joshua Onwuemene <emjay6299@gmail.com>
2022-10-29 01:41:51 +0200

    Update server.js

--- Simple_HTML_server/server.js
+++ Simple_HTML_server/server.js
@@ -7,33 +7,25 @@ let port = 3000;
 let homePath = path.join(__dirname, "./Homepage.html");
 let aboutPath = path.join(__dirname, "./about_page.html");
 let contactPath = path.join(__dirname, "./contact_page.html");
-let imagePath = path.join(__dirname, "./server_image.jpg");
 
 var server = http.createServer((req, res) => {
     let url = req.url;
     
-    if(url === "/Homepage.html"){
+    if(url === "/"){
         fs.readFile(homePath, (err, data)=>{
             if(err) throw err;
             res.writeHead(200, {"content-type":"text/html"})
             res.write(data);
             res.end();
         })
-    } else if(url === "/Homepage.html"){
-        fs.readFile(homePath, (err, data)=>{
-            if(err) throw err;
-            res.writeHead(200, {"content-type":"image/jpg"})
-            res.write(data);
-            res.end();
-        })
-    } else if (url === "/about_page.html"){
+    } else if (url === "/about"){
         fs.readFile(aboutPath, (err, data)=>{
             if(err) throw err;
             res.writeHead(200, {"content-type":"text/html"})
             res.write(data);
             res.end();
         })
-    } else if (url === "/contact_page.html") {
+    } else if (url === "/contact") {
         fs.readFile(contactPath, (err, data)=>{
             if(err) throw err;
             res.writeHead(200, {"content-type":"text/html"})
