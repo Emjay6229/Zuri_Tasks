ec231726d7e5485fec650b7b804158d27339edec
Joshua Onwuemene <emjay6299@gmail.com>
2023-01-05 22:01:58 +0100

    Update server.js

--- Simple_HTML_server/server.js
+++ Simple_HTML_server/server.js
@@ -12,21 +12,21 @@ var server = http.createServer((req, res) => {
     let url = req.url; // grabs the client's request url from the request object to be used in server-side logic.
     
     if(url === "/"){
-        fs.readFile(homePath, (err, data)=>{
+        fs.readFile(homePath, (err, data) => {
             if(err) throw err;
             res.writeHead(200, {"content-type":"text/html"})
             res.write(data);
             res.end();
         })
     } else if (url === "/about"){
-        fs.readFile(aboutPath, (err, data)=>{
+        fs.readFile(aboutPath, (err, data) => {
             if(err) throw err;
             res.writeHead(200, {"content-type":"text/html"})
             res.write(data);
             res.end();
         })
     } else if (url === "/contact") {
-        fs.readFile(contactPath, (err, data)=>{
+        fs.readFile(contactPath, (err, data) => {
             if(err) throw err;
             res.writeHead(200, {"content-type":"text/html"})
             res.write(data);
@@ -42,17 +42,6 @@ var server = http.createServer((req, res) => {
 server.listen(port, () => {
     console.log(`Server is listening at port ${port}...`)
 }) 
-
-/* In express.js, res.sendFile is use to read and send file to the client.
-SendFile can also take an err-data handler function. 
-
-e.g
-
-res.sendFile(absolute_filePath, (err, data)=>{
-            if(err) 
-                throw err;
-            res.send(data);
-        }) */
 
 
 module.exports = { server }
